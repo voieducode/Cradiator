eb09333918d6f56d8b816d94f8c0ce9be8cc6ce5
unknown <dvb@DUBWS181.renre.com>
2012-02-07 10:26:49 +0000

    Added handling for urls with UserInfo e.g. http://user:foo@blah.com

--- src/Cradiator/Services/HttpWebClient.cs
+++ src/Cradiator/Services/HttpWebClient.cs
@@ -3,18 +3,38 @@ using System.Net;
 
 namespace Cradiator.Services
 {
-	public class HttpWebClient : IWebClient
-	{
-		readonly WebClient _webClient;
+    public class HttpWebClient : IWebClient
+    {
+        readonly WebClient _webClient;
 
-		public HttpWebClient()
-		{
-			_webClient = new WebClient();
-		}
+        public HttpWebClient()
+        {
+            _webClient = new WebClient();
+        }
 
-		public string DownloadString(string url)
-		{
-			return _webClient.DownloadString(new Uri(url));
-		}
-	}
+        public HttpWebClient(Uri url)
+        {
+            _webClient = new WebClient();
+            if (!String.IsNullOrEmpty(url.UserInfo))
+            {
+                BasicAuthentication(url, _webClient);
+            }
+        }
+
+        public string DownloadString(string url)
+        {
+            return _webClient.DownloadString(new Uri(url));
+        }
+
+        protected void BasicAuthentication(Uri uri, WebClient wc)
+        {
+            var username = uri.UserInfo.Split(':')[0];
+            var password = uri.UserInfo.Split(':')[1];
+
+            var myCred = new NetworkCredential(username, password);
+            var myCache = new CredentialCache { { uri, "Basic", myCred } };
+
+            wc.Credentials = myCache;
+        }
+    }
 }
